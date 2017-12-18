164eab8a1e55c4be62c3ced9ae247d8154216210
James Pogran <james.pogran@puppet.com>
2017-12-18 13:45:46 -0500

    (MODULES-5842) Implement Generic DSC Invoker
    This commit adds the `dsc` type and provider, which allows executing
    DSC Resources without the build system used by the previous pupppet dsc
    module.
    
    This is accomplished by adding a new type that accepts a DSC Resource
    Name and Module Name, and a un-typed hash of properties. This is passed
    as is to `Invoke-DscResource`.
    
    Unit tests for the type are added, manual verification of these changes
    are to be done as automated tests are to be added in subsequent tickets

---
+++ spec/unit/puppet/type/dsc_spec.rb
@@ -0,0 +1,121 @@
+require 'spec_helper'
+require 'puppet/type'
+require 'puppet/type/dsc'
+
+describe Puppet::Type.type(:dsc) do
+  let(:resource) { described_class.new(:name => "dsc") }
+  subject { resource }
+
+  it { is_expected.to be_a_kind_of Puppet::Type::Dsc }
+
+  describe "parameter :name" do
+    subject { resource.parameters[:name] }
+
+    it { is_expected.to be_isnamevar }
+
+    it "should not allow nil" do
+      expect {
+        resource[:name] = nil
+      }.to raise_error(Puppet::Error, /Got nil value for name/)
+    end
+
+    it "should not allow empty" do
+      expect {
+        resource[:name] = ''
+      }.to raise_error(Puppet::ResourceError, /A non-empty name must/)
+    end
+
+    [ 'value', 'value with spaces', 'UPPER CASE', '0123456789_-', 'With.Period' ].each do |value|
+      it "should accept '#{value}'" do
+        expect { resource[:name] = value }.not_to raise_error
+      end
+    end
+
+    [ '*', '()', '[]', '!@' ].each do |value|
+      it "should reject '#{value}'" do
+        expect { resource[:name] = value }.to raise_error(Puppet::ResourceError, /is not a valid name/)
+      end
+    end
+  end
+
+  describe "parameter :dsc_resource_name" do
+    subject { resource.parameters[:dsc_resource_name] }
+    
+    it "should not allow nil" do
+      expect {
+        resource[:name] = nil
+      }.to raise_error(Puppet::Error, /Got nil value for name/)
+    end
+
+    it "should not allow empty" do
+      expect {
+        resource[:name] = ''
+      }.to raise_error(Puppet::ResourceError, /A non-empty name must/)
+    end
+    
+    [ 'value', 'value with spaces', 'UPPER CASE', '0123456789_-', 'With.Period' ].each do |value|
+      it "should accept '#{value}'" do
+        expect { resource[:name] = value }.not_to raise_error
+      end
+    end
+
+    [ '*', '()', '[]', '!@' ].each do |value|
+      it "should reject '#{value}'" do
+        expect { resource[:name] = value }.to raise_error(Puppet::ResourceError, /is not a valid name/)
+      end
+    end
+  end
+  
+  describe "parameter :dsc_resource_module" do
+    subject { resource.parameters[:dsc_resource_module] }
+    
+    it "should not allow nil" do
+      expect {
+        resource[:name] = nil
+      }.to raise_error(Puppet::Error, /Got nil value for name/)
+    end
+
+    it "should not allow empty" do
+      expect {
+        resource[:name] = ''
+      }.to raise_error(Puppet::ResourceError, /A non-empty name must/)
+    end
+    
+    [ 'value', 'value with spaces', 'UPPER CASE', '0123456789_-', 'With.Period' ].each do |value|
+      it "should accept '#{value}'" do
+        expect { resource[:name] = value }.not_to raise_error
+      end
+    end
+
+    [ '*', '()', '[]', '!@' ].each do |value|
+      it "should reject '#{value}'" do
+        expect { resource[:name] = value }.to raise_error(Puppet::ResourceError, /is not a valid name/)
+      end
+    end
+  end
+  
+  describe "parameter :dsc_resource_properties" do
+    subject { resource.parameters[:dsc_resource_properties] }
+    
+    it "should not allow nil" do
+      expect {
+        resource[:dsc_resource_properties] = nil
+      }.to raise_error(Puppet::Error, /Got nil value for dsc_resource_properties/)
+    end
+
+    it "should not allow empty" do
+      expect {
+        resource[:dsc_resource_properties] = ''
+      }.to raise_error(Puppet::ResourceError, /A non-empty dsc_resource_properties must be specified/)
+    end
+    
+    it "requires a hash or array of hashes" do
+      expect {
+        resource[:dsc_resource_properties] = "hi"
+      }.to raise_error(Puppet::Error, /dsc_resource_properties should be a Hash/)
+      expect {
+        resource[:dsc_resource_properties] = ["hi"]
+      }.to raise_error(Puppet::Error, /dsc_resource_properties should be a Hash/)
+    end
+  end
+end
