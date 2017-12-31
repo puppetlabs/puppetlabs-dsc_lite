cc4d4c710ed5a8786e50417dbcc831805aab6441
Iristyle <Iristyle@github>
2017-12-31 15:03:16 -0800

    (MODULES-6132) Rewrite some_failing_dsc_resources
     - Remove dependency on dsc_file / replace with dsc_puppetfakeresource
       in the some failing resource test, now that the DSC resource
       can be made to fail for the sake of the tests

--- tests/acceptance/tests/basic_dsc_resources/some_failing_dsc_resources.rb
+++ tests/acceptance/tests/basic_dsc_resources/some_failing_dsc_resources.rb
@@ -1,39 +1,41 @@
 require 'dsc_utils'
+require 'securerandom'
 test_name 'FM-2624 - C68533 - Apply DSC Resource Manifest with Mix of Passing and Failing DSC Resources'
 
 confine(:to, :platform => 'windows')
 
-# Init
-test_dir_name = 'test'
-
 # In-line Manifest
-test_dir_good = "C:/#{test_dir_name}"
-test_dir_bad = "Q:/not/here"
+throw_message = SecureRandom.uuid
 
 dsc_manifest = <<-MANIFEST
-dsc_file {'good_test_dir':
+dsc_puppetfakeresource {'good_resource':
   dsc_ensure          => 'present',
-  dsc_type            => 'Directory',
-  dsc_destinationpath => '#{test_dir_good}'
+  dsc_importantstuff  => 'foo',
 }
-dsc_file {'bad_test_dir':
+
+dsc_puppetfakeresource {'throw_resource':
   dsc_ensure          => 'present',
-  dsc_type            => 'Directory',
-  dsc_destinationpath => '#{test_dir_bad}'
+  dsc_importantstuff  => 'bar',
+  dsc_throwmessage    => '#{throw_message}',
 }
 MANIFEST
 
 # Verify
-error_msg = /Error: The system cannot find the path specified. The related file\/directory is: Q:\/not\/here/m
+error_msg = /Error: PowerShell DSC resource PuppetFakeResource  failed to execute Set-TargetResource functionality with error message: #{throw_message}/
 
 # Teardown
 teardown do
   step 'Remove Test Artifacts'
-  on(agents, "rm -rf /cygdrive/c/#{test_dir_name}")
+  agents.each do |agent|
+    uninstall_fake_reboot_resource(agent)
+  end
 end
 
 # Tests
 agents.each do |agent|
+  step 'Copy Test Type Wrappers'
+  install_fake_reboot_resource(agent)
+
   step 'Apply Manifest'
   on(agent, puppet('apply'), :stdin => dsc_manifest, :acceptable_exit_codes => 0) do |result|
     assert_match(error_msg, result.stderr, 'Expected error was not detected!')
