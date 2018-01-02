aefbde8e02463d3f52c005497c27cb134a90fcf1
Iristyle <Iristyle@github>
2018-01-02 11:34:32 -0800

    (MODULES-6132) Rewrite test_file_path.pp.erb tests
     - Files that rely on test_file_path.pp.erb currently rely on the use
       of dsc_file.
    
       Rewrite the manifest so that the parent directory is created with
       the default Puppet file resource, and that the file contents,
       which will be verified in tests, are written with the fake test
       fixture resource dsc_puppetfakeresource.
    
     - Note that the manifest is changed slightly so that:
    
         - The name of our fake resource is parameterized (so that Puppet
           apply output may be checked for a unique resource name)
         - The file path for the write of 'ImportantStuff' to
           dsc_puppetfakeresource may be set if the calling test assigns
           a value to `test_file_path` - otherwise it defaults to
           "c:\\#{test_dir_path}\\#{fake_name}"
    
       This includes the following "acceptance" (masterless) tests:
    
       * basic_functionality/alternate_path_separator
       * basic_functionality/puppet_apply_debug_dsc_manifest
       * basic_functionality/puppet_apply_dsc_manifest
       * basic_functionality/puppet_apply_noop_dsc_manifest
    
       And the following "integration" (master + agent) tests:
    
       * basic_functionality/puppet_agent_debug_dsc_manifest
       * basic_functionality/puppet_agent_dsc_manifest
       * basic_functionality/puppet_agent_noop_dsc_manifest
       * basic_functionality/negative/dsc_on_linux
          this test was updated to use a simpler inline manifest rather
          than the ERB template which included extraneous details

--- tests/acceptance/tests/basic_functionality/puppet_apply_dsc_manifest.rb
+++ tests/acceptance/tests/basic_functionality/puppet_apply_dsc_manifest.rb
@@ -1,17 +1,17 @@
 require 'erb'
 require 'dsc_utils'
+require 'securerandom'
 test_name 'FM-2625 - C68511 - Apply DSC Resource Manifest via "puppet apply"'
 
 confine(:to, :platform => 'windows')
 
 # Init
-test_dir_name = 'test'
 local_files_root_path = ENV['MANIFESTS'] || 'tests/manifests'
 
 # ERB Manifest
-test_dir_path = "C:/#{test_dir_name}"
-test_file_path = "#{test_dir_path}/test.txt"
-test_file_contents = 'catcat'
+test_dir_path = SecureRandom.uuid
+fake_name = SecureRandom.uuid
+test_file_contents = SecureRandom.uuid
 
 dsc_manifest_template_path = File.join(local_files_root_path, 'basic_functionality', 'test_file_path.pp.erb')
 dsc_manifest = ERB.new(File.read(dsc_manifest_template_path)).result(binding)
@@ -19,22 +19,26 @@ dsc_manifest = ERB.new(File.read(dsc_manifest_template_path)).result(binding)
 # Teardown
 teardown do
   step 'Remove Test Artifacts'
-  on(agents, "rm -rf /cygdrive/c/#{test_dir_name}")
+  on(agents, "rm -rf /cygdrive/c/#{test_dir_path}")
+  agents.each do |agent|
+    uninstall_fake_reboot_resource(agent)
+  end
 end
 
 # Tests
 agents.each do |agent|
+  step 'Copy Test Type Wrappers'
+  install_fake_reboot_resource(agent)
+
   step 'Apply Manifest'
   on(agent, puppet('apply'), :stdin => dsc_manifest, :acceptable_exit_codes => [0,2]) do |result|
     assert_no_match(/Error:/, result.stderr, 'Unexpected error was detected!')
+    assert_match(/Stage\[main\]\/Main\/Dsc_puppetfakeresource\[#{fake_name}\]\/ensure\: created/, result.stdout, 'DSC Resource missing!')
   end
 
   step 'Verify Results'
-  assert_dsc_resource(
-    agent,
-    'File',
-    'PSDesiredStateConfiguration',
-    :DestinationPath => test_file_path,
-    :Contents => test_file_contents
-  )
+  # PuppetFakeResource always overwrites file at this path
+  on(agent, "cat /cygdrive/c/#{test_dir_path}/#{fake_name}", :acceptable_exit_codes => [0]) do |result|
+    assert_match(/#{test_file_contents}/, result.stdout, 'PuppetFakeResource File contents incorrect!')
+  end
 end
--- tests/acceptance/tests/basic_functionality/puppet_apply_noop_dsc_manifest.rb
+++ tests/acceptance/tests/basic_functionality/puppet_apply_noop_dsc_manifest.rb
@@ -1,36 +1,40 @@
 require 'erb'
 require 'dsc_utils'
+require 'securerandom'
 test_name 'FM-2623 - C68509 - Apply DSC Resource Manifest in "noop" Mode Using "puppet apply"'
 
 confine(:to, :platform => 'windows')
 
 # Init
-test_dir_name = 'test'
 local_files_root_path = ENV['MANIFESTS'] || 'tests/manifests'
 
 # ERB Manifest
-test_dir_path = "C:/#{test_dir_name}"
-test_file_path = "#{test_dir_path}/test.txt"
-test_file_contents = 'catcat'
+test_dir_path = SecureRandom.uuid
+fake_name = SecureRandom.uuid
+test_file_contents = SecureRandom.uuid
 
 dsc_manifest_template_path = File.join(local_files_root_path, 'basic_functionality', 'test_file_path.pp.erb')
 dsc_manifest = ERB.new(File.read(dsc_manifest_template_path)).result(binding)
 
+# Teardown
+teardown do
+  agents.each do |agent|
+    step 'Remove Test Artifacts'
+    uninstall_fake_reboot_resource(agent)
+  end
+end
+
 # Tests
 agents.each do |agent|
+  step 'Copy Test Type Wrappers'
+  install_fake_reboot_resource(agent)
+
   step 'Apply Manifest'
   on(agent, puppet('apply --noop'), :stdin => dsc_manifest, :acceptable_exit_codes => [0,2]) do |result|
     assert_no_match(/Error:/, result.stderr, 'Unexpected error was detected!')
   end
 
   step 'Verify that No Changes were Made'
-  expect_failure('Expect failure because nothing should have changed') do
-    assert_dsc_resource(
-      agent,
-      'File',
-      'PSDesiredStateConfiguration',
-      :DestinationPath => test_file_path,
-      :Contents => test_file_contents
-    )
-  end
+  # if this file exists, noop didn't work
+  on(agent, "test -f /cygdrive/c/#{test_dir_path}/#{fake_name}", :acceptable_exit_codes => [1])
 end
