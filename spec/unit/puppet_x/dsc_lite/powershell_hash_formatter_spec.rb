64feb9bbdd9799a280c86c79c1ed0057d8207d63
Lukas Audzevicius <laudzevicius24@hotmail.com>
2024-05-28 14:38:10 +0100

    Correct Layout/HeredocIndentation

--- spec/unit/puppet_x/dsc_lite/powershell_hash_formatter_spec.rb
+++ spec/unit/puppet_x/dsc_lite/powershell_hash_formatter_spec.rb
@@ -14,11 +14,11 @@ describe PuppetX::PuppetLabs::DscLite::PowerShellHashFormatter do
   describe 'formatting ruby hash to powershell hash string' do
     describe 'when given correct hash' do
       it 'outputs correct syntax with simple example' do
-        expected = <<-HERE
-@{
-'ensure' = 'present';
-'name' = 'Web-WebServer'
-}
+        expected = <<~HERE
+          @{
+          'ensure' = 'present';
+          'name' = 'Web-WebServer'
+          }
         HERE
         result = formatter.format('ensure' => 'present',
                                   'name' => 'Web-WebServer')
@@ -26,19 +26,19 @@ describe PuppetX::PuppetLabs::DscLite::PowerShellHashFormatter do
       end
 
       it 'outputs correct syntax with single CimInstance for a CimInstance[] type' do
-        expected = <<-HERE
-@{
-'ensure' = 'Present';
-'name' = 'foo';
-'state' = 'Started';
-'physicalpath' = 'c:/inetpub/wwwroot';
-'bindinginfo' = [CimInstance[]]@(
-(New-CimInstance -ClassName 'MSFT_xWebBindingInformation' -ClientOnly -Property @{
-'protocol' = 'HTTP';
-'port' = 80
-})
-)
-}
+        expected = <<~HERE
+          @{
+          'ensure' = 'Present';
+          'name' = 'foo';
+          'state' = 'Started';
+          'physicalpath' = 'c:/inetpub/wwwroot';
+          'bindinginfo' = [CimInstance[]]@(
+          (New-CimInstance -ClassName 'MSFT_xWebBindingInformation' -ClientOnly -Property @{
+          'protocol' = 'HTTP';
+          'port' = 80
+          })
+          )
+          }
         HERE
 
         result = formatter.format('ensure' => 'Present',
@@ -57,19 +57,19 @@ describe PuppetX::PuppetLabs::DscLite::PowerShellHashFormatter do
       end
 
       it 'outputs correct syntax with single CimInstance for a CimInstance type' do
-        expected = <<-HERE
-@{
-'ensure' = 'Present';
-'name' = 'foo';
-'state' = 'Started';
-'physicalpath' = 'c:/inetpub/wwwroot';
-'authenticationinfo' = [CimInstance](New-CimInstance -ClassName 'MSFT_xWebAuthenticationInformation' -ClientOnly -Property @{
-'anonymous' = $true;
-'basic' = $false;
-'windows' = $true;
-'digest' = $false
-})
-}
+        expected = <<~HERE
+          @{
+          'ensure' = 'Present';
+          'name' = 'foo';
+          'state' = 'Started';
+          'physicalpath' = 'c:/inetpub/wwwroot';
+          'authenticationinfo' = [CimInstance](New-CimInstance -ClassName 'MSFT_xWebAuthenticationInformation' -ClientOnly -Property @{
+          'anonymous' = $true;
+          'basic' = $false;
+          'windows' = $true;
+          'digest' = $false
+          })
+          }
         HERE
 
         result = formatter.format('ensure' => 'Present',
@@ -90,26 +90,26 @@ describe PuppetX::PuppetLabs::DscLite::PowerShellHashFormatter do
       end
 
       it 'outputs correct syntax with array CimInstance' do
-        expected = <<-HERE
-@{
-'ensure' = 'Present';
-'name' = 'foo';
-'state' = 'Started';
-'physicalpath' = 'c:/inetpub/wwwroot';
-'bindinginfo' = [CimInstance[]]@(
-(New-CimInstance -ClassName 'MSFT_xWebBindingInformation' -ClientOnly -Property @{
-'protocol' = 'HTTP';
-'port' = 80
-}),
-(New-CimInstance -ClassName 'MSFT_xWebBindingInformation' -ClientOnly -Property @{
-'protocol' = 'HTTPS';
-'port' = 443;
-'certificatethumbprint' = '5438DC0CB31B1C91B8945C7D91B3338F9C08BEFA';
-'certificatestorename' = 'My';
-'ipaddress' = '*'
-})
-)
-}
+        expected = <<~HERE
+          @{
+          'ensure' = 'Present';
+          'name' = 'foo';
+          'state' = 'Started';
+          'physicalpath' = 'c:/inetpub/wwwroot';
+          'bindinginfo' = [CimInstance[]]@(
+          (New-CimInstance -ClassName 'MSFT_xWebBindingInformation' -ClientOnly -Property @{
+          'protocol' = 'HTTP';
+          'port' = 80
+          }),
+          (New-CimInstance -ClassName 'MSFT_xWebBindingInformation' -ClientOnly -Property @{
+          'protocol' = 'HTTPS';
+          'port' = 443;
+          'certificatethumbprint' = '5438DC0CB31B1C91B8945C7D91B3338F9C08BEFA';
+          'certificatestorename' = 'My';
+          'ipaddress' = '*'
+          })
+          )
+          }
         HERE
 
         result = formatter.format('ensure' => 'Present',
@@ -137,18 +137,18 @@ describe PuppetX::PuppetLabs::DscLite::PowerShellHashFormatter do
       end
 
       it 'outputs correct syntax with a PSCredential' do
-        expected = <<-HERE
-@{
-'username' = 'jane-doe';
-'description' = 'Jane Doe user';
-'ensure' = 'present';
-'password' = ([PSCustomObject]@{
-'user' = 'jane-doe';
-'password' = 'jane-password'
-} | new-pscredential);
-'passwordneverexpires' = $false;
-'disabled' = $true
-}
+        expected = <<~HERE
+          @{
+          'username' = 'jane-doe';
+          'description' = 'Jane Doe user';
+          'ensure' = 'present';
+          'password' = ([PSCustomObject]@{
+          'user' = 'jane-doe';
+          'password' = 'jane-password'
+          } | new-pscredential);
+          'passwordneverexpires' = $false;
+          'disabled' = $true
+          }
         HERE
         result = formatter.format('username' => 'jane-doe',
                                   'description' => 'Jane Doe user',
@@ -166,15 +166,15 @@ describe PuppetX::PuppetLabs::DscLite::PowerShellHashFormatter do
       end
 
       it 'outputs correct syntax with MSFT_KeyValuePair' do
-        expected = <<-HERE
-@{
-'destinationPath' = 'c:\fileName.jpg';
-'uri' = 'http://www.contoso.com/image.jpg';
-'userAgent' = '[Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer';
-'headers' = @{
-'Accept-Language' = 'en-US'
-}
-}
+        expected = <<~HERE
+          @{
+          'destinationPath' = 'c:\fileName.jpg';
+          'uri' = 'http://www.contoso.com/image.jpg';
+          'userAgent' = '[Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer';
+          'headers' = @{
+          'Accept-Language' = 'en-US'
+          }
+          }
         HERE
         result = formatter.format('destinationPath' => "c:\fileName.jpg",
                                   'uri' => 'http://www.contoso.com/image.jpg',
@@ -186,18 +186,18 @@ describe PuppetX::PuppetLabs::DscLite::PowerShellHashFormatter do
       end
 
       it 'outputs correct syntax with Sensitive MSFT_Credential' do
-        expected = <<-HERE
-@{
-'username' = 'jane-doe';
-'description' = 'Jane Doe user';
-'ensure' = 'present';
-'password' = ([PSCustomObject]@{
-'user' = 'jane-doe';
-'password' = 'password' # PuppetSensitive
-} | new-pscredential);
-'passwordneverexpires' = $false;
-'disabled' = $true
-}
+        expected = <<~HERE
+          @{
+          'username' = 'jane-doe';
+          'description' = 'Jane Doe user';
+          'ensure' = 'present';
+          'password' = ([PSCustomObject]@{
+          'user' = 'jane-doe';
+          'password' = 'password' # PuppetSensitive
+          } | new-pscredential);
+          'passwordneverexpires' = $false;
+          'disabled' = $true
+          }
         HERE
         sensitive_pass = Puppet::Pops::Types::PSensitiveType::Sensitive.new('password')
         result = formatter.format('username' => 'jane-doe',
