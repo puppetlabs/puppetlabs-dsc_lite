ac2cf358b8a2941e097b32d25f6a78a63e27b7d0
Lukas Audzevicius <laudzevicius24@hotmail.com>
2025-01-15 12:22:53 +0000

    p2

--- lib/puppet/provider/base_dsc_lite/powershell.rb
+++ lib/puppet/provider/base_dsc_lite/powershell.rb
@@ -7,7 +7,7 @@ require_relative '../../../puppet_x/puppetlabs/dsc_lite/powershell_hash_formatte
 Puppet::Type.type(:base_dsc_lite).provide(:powershell) do
   confine feature: :pwshlib
   confine 'os.name' => Debian
-  defaultfor 'os.name' => Debian
+  # defaultfor 'os.name' => Debian
 
   commands powershell: (if File.exist?("#{ENV.fetch('SYSTEMROOT', nil)}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe")
                           "#{ENV.fetch('SYSTEMROOT', nil)}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe"
